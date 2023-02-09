9a7c887fed13c63761a5f347b28c9e6a8590c527
redteampanda-ng <31235211+redteampanda-ng@users.noreply.github.com>
2023-02-09 15:41:32 +0100

    refactor: add logo and favicon, change navbar color

--- conf.py
+++ conf.py
@@ -80,6 +80,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_logo = "images/html/aurora-logo.png"
+html_favicon = "images/html/favicon.ico"
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
